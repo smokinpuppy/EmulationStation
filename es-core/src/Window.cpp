3751e2bd32a8d4b57bede4333078d72dae734179
Robert Haddad <rha2180@yahoo.com>
2015-02-08 17:21:39 +0200

    Update Window.cpp

--- es-core/src/Window.cpp
+++ es-core/src/Window.cpp
@@ -328,7 +328,7 @@ void Window::onSleep()
 	Renderer::setMatrix(Eigen::Affine3f::Identity());
 	unsigned char opacity = Settings::getInstance()->getString("ScreenSaverBehavior") == "dim" ? 0xA0 : 0xFF;
 	if(Settings::getInstance()->getString("ScreenSaverBehavior") == "video"){
-		system("./home/pi/playvid");
+		system("playvid");
 	} else {
 		Renderer::drawRect(0, 0, Renderer::getScreenWidth(), Renderer::getScreenHeight(), 0x00000000 | opacity);
 	}
